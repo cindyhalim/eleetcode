666131340b24f7b7461f9523f0ed851d9277550f
Cindy Halim <jcindyhalim@gmail.com>
2022-08-06 18:17:01 -0400

    initial commit

---
+++ styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  breakpoints: ["425px", "768px", "1170px", "1280px", "1440px"],
+  colors: {
+    white: "#fcfafa;",
+    black: "#0D1821",
+    mustard: "#E8AA14",
+  },
+  heading: {
+    fontSize: [26, 40, 80],
+    fontWeight: 700,
+  },
+  button: {},
+  text: {},
+};
