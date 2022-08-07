621a10681020e206d92cc2a16ec50e61cede0d68
Cindy Halim <jcindyhalim@gmail.com>
2022-08-06 21:41:11 -0400

    refactor: make response type shared

---
+++ api/types.ts
@@ -0,0 +1,16 @@
+import { HTML } from "../gql";
+
+type ProblemUrl = `https://leetcode.com/problems/${string}`;
+export interface IProblem {
+  id: string;
+  title: string;
+  difficulty: string; // "Medium" | "Easy" | "Hard";
+  url: ProblemUrl;
+  content: HTML;
+  topicTags: string[];
+}
+
+export type ProblemData = {
+  problem?: IProblem | null;
+  error?: string;
+};
--- pages/api/problem.ts
+++ pages/api/problem.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { IProblem, ProblemData } from "../../api";
 import {
-  type HTML,
   type IProblemSetQuery,
   type IQuestionDataQuery,
   type ITotalProblemsQuery,
@@ -10,73 +10,66 @@ import {
   QuestionDataQuery,
 } from "../../gql";
 
-type ProblemUrl = `https://leetcode.com/problems/${string}`;
-export interface IProblem {
-  id: string;
-  title: string;
-  difficulty: string; // "Medium" | "Easy" | "Hard";
-  url: ProblemUrl;
-  content: HTML;
-  topicTags: string[];
-}
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ProblemData>
+) => {
+  if (req.method === "GET") {
+    // since list response is limited by default, we need to get the total problems based on filter
+    const { data } = await client
+      .query<ITotalProblemsQuery>(TotalProblemsQuery, {
+        categorySlug: "",
+        filters: {},
+      })
+      .toPromise();
 
-type Data = {
-  problem: IProblem | null;
-};
+    const totalProblems = data?.problemQuestionlist?.total || 0;
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // since list response is limited by default, we need to get the total problems based on filter
-  const { data } = await client
-    .query<ITotalProblemsQuery>(TotalProblemsQuery, {
-      categorySlug: "",
-      filters: {},
-    })
-    .toPromise();
+    if (!totalProblems) {
+      return null;
+    }
 
-  const totalProblems = data?.problemQuestionlist?.total || 0;
+    const problemsList = await client
+      .query<IProblemSetQuery>(ProblemSetQuery, {
+        categorySlug: "",
+        filters: {},
+        limit: totalProblems,
+      })
+      .toPromise();
 
-  if (!totalProblems) {
-    return null;
+    // additional filters
+    const additionallyFilteredProblemsList = [
+      ...(problemsList.data?.problemsetQuestionList?.questions ?? []),
+    ].filter((question: any) => !question.paidOnly);
+
+    const randomIndex = Math.floor(
+      Math.random() * (additionallyFilteredProblemsList.length - 1)
+    );
+    const chosenProblem = additionallyFilteredProblemsList[randomIndex];
+
+    const { data: chosenProblemData } = await client
+      .query<IQuestionDataQuery>(QuestionDataQuery, {
+        titleSlug: chosenProblem.titleSlug,
+      })
+      .toPromise();
+
+    const topicTags = [...(chosenProblemData?.question?.topicTags ?? [])].map(
+      (tag: any) => tag.name
+    );
+
+    const problem: IProblem = {
+      id: chosenProblemData?.question.questionFrontendId || "",
+      title: chosenProblemData?.question.title || "",
+      difficulty: chosenProblemData?.question.difficulty || "",
+      url: `https://leetcode.com/problems/${chosenProblemData?.question?.titleSlug}`,
+      content: chosenProblemData?.question.content || "",
+      topicTags,
+    };
+
+    res.status(200).json({ problem });
+  } else {
+    res.status(500).send({ error: "Failed to fetch data" });
   }
-
-  const problemsList = await client
-    .query<IProblemSetQuery>(ProblemSetQuery, {
-      categorySlug: "",
-      filters: {},
-      limit: totalProblems,
-    })
-    .toPromise();
-
-  // additional filters
-  const additionallyFilteredProblemsList = [
-    ...(problemsList.data?.problemsetQuestionList?.questions ?? []),
-  ].filter((question: any) => !question.paidOnly);
-
-  const randomIndex = Math.floor(
-    Math.random() * (additionallyFilteredProblemsList.length - 1)
-  );
-  const chosenProblem = additionallyFilteredProblemsList[randomIndex];
-
-  const { data: chosenProblemData } = await client
-    .query<IQuestionDataQuery>(QuestionDataQuery, {
-      titleSlug: chosenProblem.titleSlug,
-    })
-    .toPromise();
-
-  const topicTags = [...(chosenProblemData?.question?.topicTags ?? [])].map(
-    (tag: any) => tag.name
-  );
-
-  const problem: IProblem = {
-    id: chosenProblemData?.question.questionFrontendId || "",
-    title: chosenProblemData?.question.title || "",
-    difficulty: chosenProblemData?.question.difficulty || "",
-    url: `https://leetcode.com/problems/${chosenProblemData?.question?.titleSlug}`,
-    content: chosenProblemData?.question.content || "",
-    topicTags,
-  };
-
-  res.status(200).json({ problem });
 };
 
 export default handler;
