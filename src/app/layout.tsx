ad4938dffe7e4603b3fbd9dfdd65056a7dc70299
Cindy Halim <jcindyhalim@gmail.com>
2023-11-28 20:41:16 -0500

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import AppProvider from './context/AppContext'
+import DrawerProvider from './context/DrawerContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,9 @@ export const metadata: Metadata = {
   title: 'eleetcode',
   description:
     'daily leetcode practice with timer to get you consistent at solving algo problems!',
+  icons: {
+    icon: './favicon.svg',
+  },
 }
 
 export default function RootLayout({
@@ -19,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppProvider>{children}</AppProvider>
+        <AppProvider>
+          <DrawerProvider>{children}</DrawerProvider>
+        </AppProvider>
       </body>
     </html>
   )
